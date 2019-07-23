3c0c5bb2991b841e028f4d7033697b6e6b233376
xgreyskiesx <greysdawn@gmail.com>
2019-07-23 17:46:32 -0400

    Increase card size and fix centering

--- frontend/src/Components/MemberCard.js
+++ frontend/src/Components/MemberCard.js
@@ -67,7 +67,8 @@ class MemberCard extends Component {
 					<input type="text" name="avatar_url" value={edit.member.avatar_url} onChange={(e)=>this.handleChange("avatar_url",e)}/>
 					<input pattern="[A-Fa-f0-9]{6}" type="text" name="color" value={edit.member.color} onChange={(e)=>this.handleChange("color",e)}/>
 					<p><input style={{width: '50px'}} type="text" name="prefix" value={edit.member.prefix} onChange={(e)=>this.handleChange("prefix",e)}/>text<input style={{width: '50px'}} type="text" name="suffix" value={edit.member.suffix} onChange={(e)=>this.handleChange("suffix",e)}/></p>
-					<p><input type="text" name="pronouns" value={edit.member.pronouns} onChange={(e)=>this.handleChange("pronouns",e)}/><br/><input type="text" pattern="\d{4}-\d{2}-\d{2}" name="birthday" value={edit.member.birthday} onChange={(e)=>this.handleChange("birthday",e)}/> </p>
+					<input type="text" name="pronouns" value={edit.member.pronouns} onChange={(e)=>this.handleChange("pronouns",e)}/>
+					<input type="text" pattern="\d{4}-\d{2}-\d{2}" name="birthday" value={edit.member.birthday} onChange={(e)=>this.handleChange("birthday",e)}/>
 					<textarea onChange={(e)=>this.handleChange("description",e)}>{edit.member.description}</textarea>
 				
 					<button className="App-button" type="submit">Save</button>
