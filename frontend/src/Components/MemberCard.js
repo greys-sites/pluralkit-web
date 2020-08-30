3f4c6edbe32bcb7f9f14fbfd51c0be64034da360
kittens <acw@ml1.net>
2020-08-30 18:12:41 -0400

    Fix formatTime where date is null

--- frontend/src/Components/MemberCard.js
+++ frontend/src/Components/MemberCard.js
@@ -62,6 +62,8 @@ class MemberCard extends Component {
 	}
 
 	formatTime = (date) => {
+		if (!date) return `null`;
+		
 		if(typeof date == "string") date = new Date(date);
 
 		return `${(date.getMonth()+1) < 10 ? "0"+(date.getMonth()+1) : (date.getMonth()+1)}.${(date.getDate()) < 10 ? "0"+(date.getDate()) : (date.getDate())}.${date.getFullYear()} at ${date.getHours() < 10 ? "0"+date.getHours() : date.getHours()}:${date.getMinutes() < 10 ? "0"+date.getMinutes() : date.getMinutes()}`
